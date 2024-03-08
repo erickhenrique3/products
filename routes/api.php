a5427dd1d7a883dd881ff614445b1e07f7f5c8fe
EduardoWisch <eduardo.wisch@escoa.pr.gov.br>
2024-03-08 20:12:28 -0300

    ok

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+// use Illuminate\Http\Request;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/teste', function (Request $request) {
+    return $request->input("age");
+});
+
+// all() - retorna todas as informaçoes independete do tipo
+
+// input()- retorna apenas os campos que vierem de input, se passar um parametro com o nome da propriedade, sera acessado p valor dela
+
+//file()  - retorna apenas dados do tipo arquivo
+
+//only() - retorna apenas os que voce passar como parâmentro
+
+//except() - retorna 
+
+
+
+
+
+// C R U D
+
+// GET   = BUSCAR DADOS
+// POST = CRIAR ALGO ENVIADO INFORMAÇOES
+// PUT = ATUAÇIZAR ALGO ENVIANDO INFORMAÇOES
+// DELETE = DELETE ALGO
