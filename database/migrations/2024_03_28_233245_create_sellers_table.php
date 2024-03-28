938ce73823f6a3c414a497b8fd7eed09bf209484
erickhenrique3 <erick.2henriquehtp@gmail.com>
2024-03-28 20:38:21 -0300

    ok

--- database/migrations/2024_03_28_233245_create_sellers_table.php
+++ database/migrations/2024_03_28_233245_create_sellers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('sellers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email'->unique());
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
